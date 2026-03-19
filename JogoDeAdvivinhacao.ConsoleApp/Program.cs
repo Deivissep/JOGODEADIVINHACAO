207e2af545f054f37e487ac191141192939afe60
Davi Gonçalves <daavi2223@gmail.com>
2026-03-19 19:31:31 -0300

    first commit

---
+++ JogoDeAdvivinhacao.ConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿//V1
+//1. Iremos fazer um jogo onde o usuário terá chances de acertar um número aleatório decidido pelo sistema.
+//2. Input (Entrada de Dados)O usuário digita número inteiro
+
+//3. Processamento
+//O sistema compara o número digitado com um número inteiro aleatório
+//Output (Saída de Dados)
+//O sistema informará o usuário se o mesmo acertou ou não, podendo incluir dicas sobre a proximidade do "chute"
+
